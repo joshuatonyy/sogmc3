72634c7b4d59ba103765bc3234d62b97fa7829c4
Joshua <joshuatony7@gmail.com>
2023-06-18 02:22:55 +0700

    Initial Commit

---
+++ sogmc3/Config/Apps/sogmc3App.swift
@@ -0,0 +1,17 @@
+//
+//  sogmc3App.swift
+//  sogmc3
+//
+//  Created by Joshua on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct sogmc3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
