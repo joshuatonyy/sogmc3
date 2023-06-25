e94085ce3933702d630aa241a00dfb4deaf6c378
Joshua <joshuatony7@gmail.com>
2023-06-25 18:41:46 +0700

    Added static transaction view

--- sogmc3/Core/Themes/Color+Theme.swift
+++ sogmc3/Core/Themes/Color+Theme.swift
@@ -22,6 +22,9 @@ extension Color {
         static let s80 = Color("main-80")
         static let s90 = Color("main-90")
         static let s100 = Color("main-100")
+        
+//        static let mainBackground = Color("mainBackground")
+//        static let mainCard = Color("mainCard")
     }
     
     struct Neutral {
